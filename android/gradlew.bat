f80fa41ecb97b80b498d609d487f23969ac2bb1f
Pollito Productions <pollitoproductions.dev@gmail.com>
2026-03-17 08:49:15 -0600

    Delete android directory

--- android/gradlew.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-cd /d "%~dp0"
-FOR /F "tokens=*" %%i IN ('java -version 2>&1') DO SET JAVA_VERSION=%%i
-echo %JAVA_VERSION%
-echo Using gradle 7.6.1
-java -XX:+IgnoreUnrecognizedVMOptions --add-modules jdk.base -cp gradle/wrapper/gradle-wrapper.jar org.gradle.wrapper.GradleWrapperMain %*
